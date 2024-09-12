97ffc7dbf4d9d294553c21f28dabbb1e7660594e
Elsacha <alexandre.gadebski@gmail.com>
2024-09-12 15:16:32 +0000

    Add Exo 3

---
+++ fiche_python_1/exo3.py
@@ -0,0 +1,12 @@
+def equation(x):
+    y1 = x**2-5
+    y2 = 3*x**2+8*x+7
+    return y1, y2
+
+
+print(equation(0))
+print(equation(1))
+print(equation(10))
+print(equation(15))
+print(equation(20))
+print(equation(25))
